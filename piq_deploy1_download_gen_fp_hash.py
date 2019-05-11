f3a402539a3f2d417579b99d1b6818dc0479748b
ken.li <ken.li@corp.to8to.com>
2019-05-11 11:25:27 +0800

    增加图片下载器类，支持批量分批次存储图片指纹

--- piq_deploy1_download_gen_fp_hash.py
+++ piq_deploy1_download_gen_fp_hash.py
@@ -16,6 +16,14 @@ def get_input_args():
                         help='start index of data')
     parser.add_argument('--size', type=int, default=10000, 
                         help='num of data')
+    parser.add_argument('--batch', type=int, default=10000, 
+                        help='batch size')
+
+    parser.add_argument('--id', type=str, default='tid', 
+                        help='batch size')
+
+    parser.add_argument('--file', type=str, default='case_struct', 
+                        help='batch size')
 
     return parser.parse_args()
 
@@ -37,11 +45,12 @@ def download_img_by_filename(filename, timeout=3, bgr=False, gray_func=None, pro
             img_ndarray = proc_func(img_ndarray)
     return img_ndarray
 
-def download_from_df(df, start=0, size=10000, timeout=1, bgr=False, gray_func=None, proc_func=None):
+def download_from_df(df, start=0, size=10000, timeout=1, bgr=False, gray_func=None, proc_func=None, id_name='tid', failed_filename=''):
     index = df.index[start: start+size]
     res = list()
+    failed_down = list()
     for i in index:
-        cid, tid, filename = df.loc[i, ['cid', 'tid', 'filename']]
+        cid, ID, filename = df.loc[i, ['cid', id_name, 'filename']]
         try:
             img_data = download_img_by_filename(filename,
                                         timeout=timeout,
@@ -49,10 +58,14 @@ def download_from_df(df, start=0, size=10000, timeout=1, bgr=False, gray_func=No
                                         gray_func=gray_func,
                                         proc_func=proc_func)
             if img_data is None:
+                failed_down.append([i, str(cid), str(ID), filename])
                 continue
-            res.append([i, str(cid), str(tid), img_data])
+            res.append([i, str(cid), str(ID), img_data])
         except:
+            failed_down.append([i, str(cid), str(ID), filename])
             continue
+    if len(failed_down) > 0:
+        saveJson(failed_down, 'data/{}_{}-{}.json'.format(failed_filename + '_down_failed', start, start+size-1))
     return res
 
 def gen_batch(start, size):
@@ -81,11 +94,47 @@ def gen_batch(start, size):
     print('cost time {}s'.format(time() - start_time))
 
     print('save images to json ...')
-    saveJson(res, 'data/case_struct_img_hash_fp.json')
+    saveJson(res, 'data/case_struct_img_hash_fp_{}-{}.json'.format(start, start+size-1))
     print('cost time {}s'.format(time() - start_time))
 
     print('completed!')
 
+class ImgDownLoader:
+    def __init__(self, id_name='tid', csv_filename='case_struct'):
+        print('init loader for {}:\n'.format(csv_filename))
+        self.imf = ImFeature(k=50)
+        self.imf_crop = CropImFeature(k=50)
+        self.imf_resize = ResizeImFeature(k=50)
+        self.bgr2gray = lambda x: cv.cvtColor(x, cv.COLOR_BGR2GRAY)
+        self.gen_fp = lambda im: self.imf.fingerprint(self.imf.feature(im)[1])
+        self.gen_hash = lambda im: imhash_dct(self.imf_resize.resize(self.imf_crop.crop(im), size=4))
+        origin_dataset = pd.read_csv('data/deploy/{}.csv'.format(csv_filename))
+        self.df = origin_dataset[origin_dataset.filename != '\\N'].sort_values(by=['cid', 'puttime'])
+        self.id_name = id_name
+        self.filename = csv_filename
+    def run(self, start, size):
+        proc_func = [self.gen_hash, self.gen_fp]
+        print('download from {} to {} ...'.format(start, start + size - 1))
+        start_time = time()
+        res = download_from_df(self.df, start=start, size=size, bgr=True, gray_func=self.bgr2gray, proc_func=proc_func, id_name=self.id_name, failed_filename=self.filename)
+        print('download cost time {} h.'.format((time() - start_time)/3600))
+        res_json = 'data/{}_img_hash_fp_{}-{}.json'.format(self.filename, start, start+size-1)
+        print('save to {}.'.format(res_json))
+        saveJson(res, res_json)
+        print('done.')
+    def run_batch(self, start=0, size=100000, batch_size=10000):
+        batch_num = size // batch_size
+        left_num = size % batch_size
+        for i in range(batch_num):
+            _start = start + batch_size * i
+            _size = batch_size
+            self.run(_start, _size)
+        if left_num > 0:
+            _start = start + batch_size * batch_num
+            _size = left_num
+            self.run(_start, _size)
+
 if __name__ == '__main__':
     in_arg = get_input_args()
-    gen_batch(in_arg.start, in_arg.size)
+    loader = ImgDownLoader(id_name=in_arg.id, csv_filename=in_arg.file)
+    loader.run_batch(start=in_arg.start, size=in_arg.size, batch_size=in_arg.batch)
